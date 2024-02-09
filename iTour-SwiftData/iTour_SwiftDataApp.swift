8f39ae479a26c1d55f6214d63f8bcf0970f1eab2
Eric <ebagnaro@gmail.com>
2024-02-09 06:01:34 +0100

    Initial Commit

---
+++ iTour-SwiftData/iTour_SwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTour_SwiftDataApp.swift
+//  iTour-SwiftData
+//
+//  Created by Eric on 09/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct iTour_SwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
