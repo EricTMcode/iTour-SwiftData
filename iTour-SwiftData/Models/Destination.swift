ffc83622cfda8a5fd665a4761843ff5d4b9b30b9
Eric <ebagnaro@gmail.com>
2024-02-09 06:05:23 +0100

    - Initial commit

---
+++ iTour-SwiftData/Models/Destination.swift
@@ -0,0 +1,24 @@
+//
+//  Destination.swift
+//  iTour-SwiftData
+//
+//  Created by Eric on 09/02/2024.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class Destination {
+    var name: String
+    var details: String
+    var date: Date
+    var priority: Int
+    
+    init(name: String = "", details: String = "" , date: Date = .now, priority: Int = 2) {
+        self.name = name
+        self.details = details
+        self.date = date
+        self.priority = priority
+    }
+}
--- iTour-SwiftData/iTour_SwiftDataApp.swift
+++ iTour-SwiftData/iTour_SwiftDataApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct iTour_SwiftDataApp: App {
@@ -13,5 +14,6 @@ struct iTour_SwiftDataApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: Destination.self)
     }
 }
